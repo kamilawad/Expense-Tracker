d32379805d18b70443b017624a283e83bcde50ba
kamilawad1 <kamilawad15@gmail.com>
2024-03-11 06:24:03 +0200

    added functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
-const currenciesContainer = document.getElementById('currencies');
+const currenciesContainer = document.getElementById('currency');
+const transactionForm = document.getElementById('transaction-form');
+const totalBalance = document.getElementById('totalbalance');
+const transactionsList = document.getElementById('transactions');
+let transactions = [];
 
 const result = fetch('https://rich-erin-angler-hem.cyclic.app/students/available');
 
 result
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         data.forEach(currency => {
             const option = document.createElement('option');
             option.value = currency.code;
@@ -23,3 +26,79 @@ result
 result.then((response) => {
    console.log(response.data);
 });*/
+
+const calculateBalance = () => {
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    transactions.forEach(transaction => {
+        if (transaction.type === 'income') {
+            totalIncome += transaction.amount;
+        } else {
+            totalExpense += transaction.amount;
+        }
+    });
+
+    const total = totalIncome - totalExpense;
+    totalBalance.textContent = `${total} USD`;
+}
+
+const displayTransactions = () => {
+    transactionsList.innerHTML = '';
+    transactions.forEach(transaction => {
+        const li = document.createElement('li');
+        if (transaction.type === 'income') {
+            li.classList.add('income');
+        }
+        else {
+            li.classList.add('expense');
+        }
+        li.textContent = `${transaction.amount} ${transaction.transactionCurrency}`;
+        transactionsList.appendChild(li);
+    });
+};
+
+const loadTransactions = () => {
+    const savedTransactions = localStorage.getItem('transactions');
+    if (savedTransactions) {
+        transactions = JSON.parse(savedTransactions);
+        calculateBalance();
+        displayTransactions();
+    }
+}
+
+const saveTransactions = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions)); 
+}
+
+transactionForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const amount = parseFloat(document.getElementById('amount').value);
+    const type = document.getElementById('type').value;
+    const transactionCurrency = document.getElementById('currency').value;
+    transactions.push({amount, type, transactionCurrency});
+    saveTransactions();
+    loadTransactions();
+    /*const userData = {
+        from: transactionCurrency,
+        to: 'USD',
+        amount: amount
+    };
+
+    fetch("https://rich-erin-angler-hem.cyclic.app/students/convert", {
+        method: "POST",
+        body: JSON.stringify(userData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const {amount} = data;
+        transactions.push(amount);
+        let total = calculateBalance(type,amount)
+
+    })
+    .catch(error => {
+        console.errlogor('Error:', error.message);
+    });*/
+
+});
+
